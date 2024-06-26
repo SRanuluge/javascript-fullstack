c91f1f1c03c1774097a6bc2898b4848b43122bf2
SRanuluge <sranuluge.dev@gmail.com>
2024-06-26 16:49:08 +1200

    node app

--- node-app/index.js
+++ node-app/index.js
@@ -3,11 +3,14 @@ const http = require("http");
 const hostname = "localhost";
 const port = 3000;
 
+var dd = "d";
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-type", "text/plain");
   res.end("Hello, Worldr", req);
 });
+
+console.log("dfdf");
 
 server.listen(port, hostname, () => {
   console.log("Server is runing at port" + port);
